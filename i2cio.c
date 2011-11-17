3d9adb3fb6c0ac7176122d2b62779748352576c0
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2011-11-17 13:06:58 +0100

    Extended by bit remapping configuration

--- i2cio.c
+++ i2cio.c
@@ -4,6 +4,7 @@
 #include <fcntl.h>
 #include <assert.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 #define I2C_SLAVE	0x0703	/* Change slave address			*/
 
@@ -12,6 +13,9 @@ int i2cio_init(struct i2cio_dev *dev, const char *path)
 	dev->fd = open(path, O_RDWR);
 	if(dev->fd == -1)
 		return -1;
+
+	for(size_t i = 0; i < 8; ++i)
+		dev->bitmap[i] = i;
 
 	return 0;
 }
@@ -30,6 +34,17 @@ int i2cio_setaddr(struct i2cio_dev *dev, uint8_t addr)
 	if(ioctl(dev->fd, I2C_SLAVE, addr & 0x7F) < 0)
 		return -1;
 
+	return 0;
+}
+
+int i2cio_setbitmap(struct i2cio_dev *dev, int bitmap[8])
+{
+	for(size_t i = 0; i < 8; ++i) {
+		if(bitmap[i] < -1 || bitmap[i] >= 8)
+			return 1;
+	}
+
+	memcpy(dev->bitmap, bitmap, 8);
 	return 0;
 }
 
--- i2cio.h
+++ i2cio.h
@@ -10,6 +10,7 @@
 
 struct i2cio_dev {
 	int fd;
+	int bitmap[8];
 };
 
 /**
@@ -26,6 +27,15 @@ void i2cio_fini(struct i2cio_dev *dev);
  * Sets the I2C device address.
  */
 int i2cio_setaddr(struct i2cio_dev *dev, uint8_t addr);
+
+/**
+ * Sets bit mappings.
+ * Remaps bits of data that is being written or read.
+ * Each bitmap[i] must be in range -1..7 where -1 means
+ * ignore that bit. When a bitmap[i] and bitmap[j] (i != j)
+ * remaps the bit to same position the result is undefined.
+ */
+int i2cio_setbitmap(struct i2cio_dev *dev, int bitmap[8]);
 
 /**
  * Writes a byte to the I2C device.
